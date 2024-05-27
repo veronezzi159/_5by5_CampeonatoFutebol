56d0d0ae4d48de44bda1c5b26fba6b5ad732220b
veronezzi159 <lucasveronezzi3@gmail.com>
2024-05-27 09:37:09 -0300

    Add project files.

---
+++ CampeonatoFutebol/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CampeonatoFutebol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
